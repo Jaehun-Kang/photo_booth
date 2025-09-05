436b9b7b836fa27e68734311ba82f2ee75b7ef2e
Jaehun-Kang <74220533+Jaehun-Kang@users.noreply.github.com>
2025-09-06 01:52:15 +0900

    index.js change(sample remove)

--- src/filters/index.js
+++ src/filters/index.js
@@ -6,9 +6,6 @@ import { templateFilter5 } from "../filters/filter_5";
 import { templateFilter6 } from "../filters/filter_6";
 import { templateFilter7 } from "../filters/filter_7";
 import { templateFilter8 } from "../filters/filter_8";
-import { circleFilter } from "../filters/filter_sample_1";
-import { fadeFilter } from "../filters/filter_sample_2";
-import { cameraView } from "../filters/filter_sample_3";
 
 /* [!중요!] 필터 선택해서 만드실 때 디스코드 포토부스 채널에 몇번 필터 파일 사용하겠다고 말씀해주세요. */
 /* 필터 함수 이름은 아래 배열의 templateFilter쪽과 위쪽 해당하는 import {} 안까지 변경해주시면 됩니다. */
@@ -21,7 +18,4 @@ export const filters = [
   templateFilter6,
   templateFilter7,
   templateFilter8,
-  circleFilter,
-  fadeFilter,
-  cameraView,
 ];
